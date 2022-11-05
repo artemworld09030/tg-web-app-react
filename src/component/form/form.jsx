3e2654dffd1ae98025cf0885b0e1870b8e473065
vladislavsevcenko <marick09030@gmail.com>
2022-11-06 02:58:37 +0300

    init

--- src/component/form/form.jsx
+++ src/component/form/form.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './form.css';
 
 const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+
+    const onChangeCounty = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
     return (
-        <div>
-            FORM
+        <div className={'form'}>
+            <h3>Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type='text'
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCounty}
+            />
+            <input
+                className={'input'}
+                type='text'
+                placeholder={'Улица'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ. Лицо</option>
+                <option value={'legal'}>Юр. Лицо</option>
+            </select>
         </div>
     );
 };
